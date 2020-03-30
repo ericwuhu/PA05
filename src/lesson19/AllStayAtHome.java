bad3c26c3e4d09ddf26803af99127a1ed8fff761
Eric Hu <62254141+ericwuhu@users.noreply.github.com>
2020-03-30 16:14:38 -0700

    Testing Commit

--- src/lesson19/AllStayAtHome.java
+++ src/lesson19/AllStayAtHome.java
@@ -5,7 +5,8 @@ public class AllStayAtHome extends Population{
   public AllStayAtHome(int n){
     super(n);
   }
-
+  
+  // Test 
   public void createPeople(){
     for(int i=0; i<this.people.length; i++){
       this.addPerson(new StayAtHome());
